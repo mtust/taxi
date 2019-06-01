243cc3aab370d1c95bd3cf66943e7d9e9f3007f8
solomiiariznychok <solomiyariznuchok@gmail.com>
2019-06-01 17:45:30 +0300

    Add test2 "Add additional verification to test."

--- src/test/java/com/tustanovskyy/taxi/TestBranchTest.java
+++ src/test/java/com/tustanovskyy/taxi/TestBranchTest.java
@@ -2,6 +2,7 @@ package com.tustanovskyy.taxi;
 
 import org.junit.Assert;
 import org.junit.BeforeClass;
+import org.junit.Test;
 
 public class TestBranchTest {
 
@@ -10,5 +11,15 @@ public class TestBranchTest {
         boolean a = true;
         Assert.assertTrue("Verify conditions", a);
         Assert.assertTrue("Verify conditions false2", false);
+    }
+
+    @Test
+    public  void verefySmth(){
+        Assert.assertFalse("Verify", true);
+    }
+
+    @Test
+    public  void verefySmth2(){
+        Assert.assertFalse("Verify", true);
     }
 }
