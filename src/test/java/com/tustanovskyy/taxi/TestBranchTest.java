d89da207f06c48dcf87d0c185b891890c9994f07
solomiiariznychok <solomiyariznuchok@gmail.com>
2019-06-01 17:03:03 +0300

    Add a new test.

---
+++ src/test/java/com/tustanovskyy/taxi/TestBranchTest.java
@@ -0,0 +1,13 @@
+package com.tustanovskyy.taxi;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+
+public class TestBranchTest {
+
+    @BeforeClass
+    public void prepareBeforeTest(){
+        boolean a = true;
+        Assert.assertTrue("Verify conditions", a);
+    }
+}
