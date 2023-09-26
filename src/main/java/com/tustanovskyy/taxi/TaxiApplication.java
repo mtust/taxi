ebfc9a1bba63f960732b909fa19b99b0f44af2e9
Myroslav <myroslavtustanovskyy@cloud.upwork.com>
2023-09-26 21:58:13 +0300

    add port config

--- src/main/java/com/tustanovskyy/taxi/TaxiApplication.java
+++ src/main/java/com/tustanovskyy/taxi/TaxiApplication.java
@@ -1,12 +1,23 @@
 package com.tustanovskyy.taxi;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
+@Slf4j
 public class TaxiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TaxiApplication.class, args);
+		String port = System.getenv("PORT");
+		if (port == null) {
+			port = "8080";
+			log.warn("$PORT environment variable not set, defaulting to 8080");
+		}
+		SpringApplication app = new SpringApplication(TaxiApplication.class);
+		app.setDefaultProperties(Collections.singletonMap("server.port", port));
+		app.run(args);
 	}
 }
