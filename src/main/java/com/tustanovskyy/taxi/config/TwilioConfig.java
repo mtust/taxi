98839b5058084c76c5af21d0eeb1bfb209297cde
myroslavt <myroslav.tustanovsky@firstgroup.com>
2024-12-08 23:16:06 +0200

    config

--- src/main/java/com/tustanovskyy/taxi/config/TwilioConfig.java
+++ src/main/java/com/tustanovskyy/taxi/config/TwilioConfig.java
@@ -2,11 +2,14 @@ package com.tustanovskyy.taxi.config;
 
 import com.twilio.Twilio;
 import lombok.Getter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class TwilioConfig {
+    private static final Logger log = LoggerFactory.getLogger(TwilioConfig.class);
     @Value("${twilio.accountSid}")
     private String accountSid;
 
@@ -18,6 +21,9 @@ public class TwilioConfig {
     private String twilioServiceId;
 
     public TwilioConfig() {
+        log.info("sid: {} ", accountSid);
+        log.info("token {} ", authToken);
+        log.info("id: {} ", twilioServiceId);
         Twilio.init(accountSid, authToken);
     }
 
